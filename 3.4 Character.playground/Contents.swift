bd44da3fbcb61a2cbda565b00d64a3bec5e23e45
ee2e <ee2e@naver.com>
2021-01-03 21:02:52 +0900

    3.4 Character

---
+++ 3.4 Character.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+let alphabetA: Character = "A"
+print(alphabetA)
+/* A */
+
+// Character 값에 유니코드 문자를 사용할 수 있음
+let commandCharacter: Character = "♡"
+print(commandCharacter)
+/* ♡ */
+
+// 한글도 유니코드 문자에 속하므로 스위프트 코드의 변수 이름으로 사용할 수 있음
+let 한글변수이름: Character = "ㄱ"
+print("한글의 첫 자음: \(한글변수이름)")
+/* 한글의 첫 자음: ㄱ */
