a395395a67a7a8b87281e34b5349a06ed2b6f930
ee2e <ee2e@naver.com>
2021-01-03 20:55:38 +0900

    3.2 Bool

---
+++ 3.2 Bool.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var boolean: Bool = true
+boolean.toggle()    // true - false 반전
+print(boolean)
+/* false */
+
+let iLoveYou: Bool = true
+let isTimeUnlimited: Bool = false
+print("시간은 무한합니까?: \(isTimeUnlimited)")
+/* 시간은 무한합니까?: false */
