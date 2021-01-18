f0e1bfa5fb1953d8a635f5caf670fd3f0fc2202a
ee2e <ee2e@naver.com>
2021-01-18 21:50:21 +0900

    5.1.2 산술 연산자

---
+++ 5.1.2 산술 연산자.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let number: Double = 5.0
+var result: Double = number.truncatingRemainder(dividingBy: 1.5)
+/* 0.5 */
+result = 12.truncatingRemainder(dividingBy: 2.5)
+/* 2.0 */
+
+var result2: Int = 5 / 3
+/* 1 */
+result2 = 10 / 3
+/* 3 */
