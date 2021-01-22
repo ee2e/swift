f95590d0fb0d1b33855713f2aff4e0f9bf8bdae7
이지은 <ee2e@naver.com>
2021-01-23 00:19:03 +0900

    6.3 구문 이름표

---
+++ 6.3 구문 이름표.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var numbers: [Int] = [3, 2342, 6, 3252]
+
+numbersLoop: for num in numbers {
+    if num > 5 || num < 1 {
+        continue numbersLoop
+    }
+    
+    var count: Int = 0
+    
+    printLoop: while true {
+        
+        print(num)
+        count += 1
+        
+        if count == num {
+            break printLoop
+        }
+    }
+    
+    removeLoop: while true {
+        if numbers.first != num {
+            break numbersLoop
+        }
+        numbers.removeFirst()
+    }
+}
+
+/*
+ 3
+ 3
+ 3
+*/
+
+// numbers에는 [2342, 6, 3252]가 남는다.
